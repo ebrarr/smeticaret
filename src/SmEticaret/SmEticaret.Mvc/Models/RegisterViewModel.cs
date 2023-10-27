8a3dd8430c1370204702f9dea9beb6cbdbb76723
ebraryanik <ebrarucar96@gmail.com>
2023-10-27 18:55:36 +0300

    added login and register views

---
+++ src/SmEticaret/SmEticaret.Mvc/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmEticaret.Mvc.Models
+{
+	public class RegisterViewModel : Controller
+	{
+		[Required]
+		public int RoleId { get; set; }
+
+		[Required, MaxLength(50)]
+		public string Name { get; set; }
+
+		[Required, MaxLength(50)]
+		public string LastName { get; set; }
+
+		[Required, EmailAddress]
+		public string Email { get; set; }
+
+		[Required, MinLength(1), DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required, MinLength(1), Compare(nameof(Password))]
+		public string PasswordRepeat { get; set; }
+	}
+}
